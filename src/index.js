a33897d1e60000dd11e55b7fb7658300f94aceba
marks-matters <markdjennings@gmail.com>
2017-06-28 00:56:41 -0400

    get star sign for dates functionality added

--- src/index.js
+++ src/index.js
@@ -9,12 +9,30 @@
  // 1. Text strings =====================================================================================================
  //    Modify these strings and messages to change the behavior of your Lambda function
 
+var data = {
+    'starSignDates': {
+        'aries':        {'fromDate': '03-21', 'toDate': '04-19'},
+        'taurus':       {'fromDate': '04-20', 'toDate': '05-20'},
+        'gemini':       {'fromDate': '05-21', 'toDate': '06-20'},
+        'cancer':       {'fromDate': '06-21', 'toDate': '07-22'},
+        'leo':          {'fromDate': '07-23', 'toDate': '08-22'},
+        'virgo':        {'fromDate': '08-23', 'toDate': '09-22'},
+        'libra':        {'fromDate': '09-23', 'toDate': '10-22'},
+        'scorpio':      {'fromDate': '10-23', 'toDate': '11-21'},
+        'sagittarius':  {'fromDate': '11-22', 'toDate': '12-21'},
+        'capricorn':    {'fromDate': '12-22', 'toDate': '01-19'},
+        'aquarius':     {'fromDate': '01-20', 'toDate': '02-18'},
+        'pisces':       {'fromDate': '02-19', 'toDate': '03-20'}
+    }
+};
+
 var speechOutput = '';
 var reprompt;
 var welcomeOutput = "Which star sign's horoscope would you like to hear?";
 var welcomeReprompt = "I didn't quite catch that, please request a star sign's horoscope, for example, Scorpio's horoscope.";
 var starSign;
 var existingStarSign;
+var compareToDate = new Date();
 
 var APP_ID = "amzn1.ask.skill.d373228d-ef5c-4a0c-a005-583c0d25bf11";
 
@@ -98,36 +116,38 @@ var handlers = {
         //delegate to Alexa to collect all the required slot values
         var filledSlots = delegateSlotCollection.call(this);
         var speechOutput = "";
-        //any intent slot variables are listed here for convenience
-        var dateSlot = this.event.request.intent.slots.date.value;
-        console.log(dateSlot);
+        compareToDate = this.event.request.intent.slots.date.value;
+        // loop through the star sign dates, if the date lies in the date range then return the relevant star sign
+        starSign = Object.keys(data.starSignDates).forEach(function(zodiacSign) {
+            dateChecker(zodiacSign);
+        });
 
-        //Your custom intent handling goes here
-        speechOutput = "This is a place holder response for the intent named GetZoidicSignFromDateIntent, which includes dialogs. This intent has one slot, which is date. Anything else?";
-        this.emit(":ask",speechOutput);
+        speechOutput = "The star sign for someone born on ";
+        reprompt = "Ask for the star sign of a different date, or, check out your star sign's horoscope.";
+        this.emit(":ask", speechOutput + compareToDate + " is " + , reprompt);
     },
     'GetCompatibleZodiacSignIntent': function () {
         //delegate to Alexa to collect all the required slot values
         var filledSlots = delegateSlotCollection.call(this);
         var speechOutput = "";
         //any intent slot variables are listed here for convenience
-        var zodiacSignASlot = this.event.request.intent.slots.zodiacSignA.value;
-        console.log(zodiacSignASlot);
-        var zodiacSignBSlot = this.event.request.intent.slots.zodiacSignB.value;
-        console.log(zodiacSignBSlot);
+        var starSignASlot = this.event.request.intent.slots.zodiacSignA.value;
+        console.log(starSignASlot);
+        var starSignBSlot = this.event.request.intent.slots.zodiacSignB.value;
+        console.log(starSignBSlot);
 
         //Your custom intent handling goes here
-        speechOutput = "This is a place holder response for the intent named GetCompatibleZodiacSignIntent, which includes dialogs. This intent has 2 slots, which are zodiacSignA, and zodiacSignB. Anything else?";
+        speechOutput = "The star sign for ";
         this.emit(":ask",speechOutput);
     },
     'SetUserZodiacSignIntent': function () {
         speechOutput = "Your star sign has been updated to ";
         reprompt = "Which star sign's horoscope would you like to hear, for example, Scorpio's horoscope or my horoscope";
-
+        // set starSign to the slot value that accompanies the SetUserZodiacSignIntent intent
         starSign = this.event.request.intent.slots.inputZodiacSign.value;
         this.attributes['existingStarSign'] = starSign;
         existingStarSign = this.attributes['existingStarSign'];
-
+        // emit the response, keep daily horoscope open
         this.emit(':ask', speechOutput + existingStarSign + '. ', reprompt);
     },
     'GetUserZodiacSignIntent': function () {
@@ -174,7 +194,6 @@ function getHoroscope(callback) {
 function readUserStarSign(callback) {
     var AWS = require('aws-sdk');
     AWS.config.update({region: AWSregion});
-
     var docClient = new AWS.DynamoDB.DocumentClient();
 
     docClient.get(params, (err, data) => {
@@ -187,20 +206,34 @@ function readUserStarSign(callback) {
     });
 }
 
-function isSlotValid(request, slotName){
-        var slot = request.intent.slots[slotName];
-        console.log("request = "+JSON.stringify(request)); //uncomment if you want to see the request
-        var slotValue;
+function dateChecker(zodiacSign) {
+    var compareYear = compareToDate.getFullYear();
+    var rawFromDate = data.starSignDates[zodiacSign].fromDate;
+    var rawToDate = data.starSignDates[zodiacSign].toDate;
 
-        //if we have a slot, get the text and store it into speechOutput
-        if (slot && slot.value) {
-            //we have a value in the slot
-            slotValue = slot.value.toLowerCase();
-            return slotValue;
-        } else {
-            //we didn't get a value in the slot.
-            return false;
-        }
+    var fromDate = new Date(Date.UTC(compareYear, rawFromDate.substr(0,2) - 1, rawFromDate.substr(3,2)));
+    var toDate = new Date(Date.UTC(compareYear, rawToDate.substr(0,2) - 1, rawToDate.substr(3,2)));
+
+    var dateRange = data.starSignDates[zodiacSign];
+    if (compareToDate >= fromDate && compareToDate <= toDate) {
+        return zodiacSign
+    }
+}
+
+function isSlotValid(request, slotName){
+    var slot = request.intent.slots[slotName];
+    console.log("request = "+JSON.stringify(request)); //uncomment if you want to see the request
+    var slotValue;
+
+    //if we have a slot, get the text and store it into speechOutput
+    if (slot && slot.value) {
+        //we have a value in the slot
+        slotValue = slot.value.toLowerCase();
+        return slotValue;
+    } else {
+        //we didn't get a value in the slot.
+        return false;
+    }
 }
 
 function delegateSlotCollection(){
